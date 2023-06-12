88b73f28d9ff823c6b0268cd93378a3f4f5aca35
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2023-06-12 13:01:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,20 +12,21 @@ const app = express();
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
+app.use(cors());
 // app.use(
 //   cors({
 //     origin: ["http://localhost:3000", "https://ydovzhyk.github.io"],
 //   })
 // );
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://ydovzhyk.github.io/easy-shop/;http://localhost:3000"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://ydovzhyk.github.io/easy-shop/;http://localhost:3000"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use(express.json());
 app.use("/static", express.static("public")); // For access a file
