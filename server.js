193cbf4297710f220448ef6223700fb88d9ee6c0
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2023-08-01 13:39:42 +0300

    add new WS server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const dialogueController = require("./controllers/dialogueController");
+const http = require("http");
+const startWebSocketServer = require("./wsserver");
 
 mongoose.set("strictQuery", false);
 const { DB_HOST, PORT = 4000 } = process.env;
@@ -8,7 +9,10 @@ const { DB_HOST, PORT = 4000 } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT, () => console.log("Server started on port", PORT));
+    // app.listen(PORT, () => console.log("Server started on port", PORT));
+    const httpServer = http.createServer(app);
+    httpServer.listen(PORT, () => console.log("Server started on port", PORT));
+    startWebSocketServer(httpServer); // Запускаємо WebSocket-сервер, передавши HTTP-сервер як параметр
   })
   .catch((error) => {
     console.log("Error", error.message);
---
+++ wsserver.js
@@ -0,0 +1,42 @@
+const { Server } = require("ws");
+const url = require("url");
+const dialogueController = require("./controllers/dialogueController");
+
+const connectedClients = {};
+
+function findConnectionIdByWebSocket(ws) {
+  return Object.keys(connectedClients).find(
+    (id) => connectedClients[id] === ws
+  );
+}
+
+function startWebSocketServer(httpServer) {
+  const wss = new Server({ server: httpServer });
+  console.log(wss);
+
+  wss.on("connection", (ws, req) => {
+    const query = url.parse(req.url, true).query;
+    const connectionId = query.user;
+    connectedClients[connectionId] = ws;
+    console.log(`Client ${connectionId} connected`);
+
+    ws.on("message", async (message) => {
+      // Код для обробки повідомлення та підготовки відповіді
+      const response = await dialogueController.checkUpdatesDialogueController(
+        JSON.parse(message.toString())
+      );
+
+      const senderId = findConnectionIdByWebSocket(ws);
+      const senderWebSocket = connectedClients[senderId];
+
+      if (senderWebSocket && senderWebSocket.readyState === 1) {
+        console.log("Зайшли відправити відповідь");
+        senderWebSocket.send(JSON.stringify(response.message));
+      }
+    });
+
+    ws.on("close", () => console.log("Client disconnected"));
+  });
+}
+
+module.exports = startWebSocketServer;
