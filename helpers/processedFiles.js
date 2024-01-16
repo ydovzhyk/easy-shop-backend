c4abac1f9f5448b26a621ae1001f355ec33cea5e
Yuriy Dovzhyk <ydovzhyk@gmail.com>
2024-01-16 12:51:35 +0200

    Update processedFiles.js

--- helpers/processedFiles.js
+++ helpers/processedFiles.js
@@ -65,7 +65,11 @@ const uploadFileToDrive = async (file, auth) => {
 
     // create direct Link to image
     const fileId = data.id;
-    const imageLink = `https://drive.google.com/uc?export=view&id=${fileId}`;
+    // const imageLink = `https://drive.google.com/uc?export=view&id=${fileId}`;
+
+    const imageLink = `https://lh3.googleusercontent.com/d/${fileId}`;
+    //lh3.googleusercontent.com/d/FILE_ID
+
     // const imageLink = `https://drive.usercontent.google.com/download?id=${fileId}&export=view&authuser=0`;
     // const imageLink = `https://drive.google.com/uc?id=${fileId}`;
     return imageLink;
